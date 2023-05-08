3bd56c25aa0bd85a0658837f87b762291ee45151
nadhemDev <blendplay2@gmail.com>
2023-05-08 22:51:32 +0100

    index page fixed

---
+++ resources/views/frontend/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+@section('content')
+    <div class="row justify-content-center">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    index
+                </div>
+            </div>
+            <div class="card-body">
+                i'am index
+            </div>
+        </div>
+    </div>
+@endsection
